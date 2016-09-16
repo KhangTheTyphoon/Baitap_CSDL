26cf8953be5105b9c4f3901e1ceb7f3dcfe30063
Lê Tiến Khang <khangthetyphoon@gmail.com>
2016-09-16 17:31:42 +0700

    Add files via upload

---
+++ baitap1ktlt.cpp
@@ -0,0 +1,31 @@
+#include "iostream"
+#include <math.h>
+using namespace std;
+void nhapso(int &a);
+float giatribieuthuc(int n);
+void main ()
+{
+	int b;
+	nhapso(b);
+	cout<<"\nGia tri bieu thuc can tinh la: "<<giatribieuthuc(b)<<endl;
+}
+void nhapso(int &a)
+{
+	do{
+		cout<<"\nNhap so nguyen duong: ";
+		cin>>a;
+	}while(a<=0);
+}
+float giatribieuthuc(int n)
+{
+	float tong=0;
+	int mau=1;
+	int dau=0;
+	for(int i=2; i<=n; i++)
+	{
+		mau=mau*i;
+		tong+=(pow((float)-1,i)/(float)mau)* dau;
+		dau=-1;
+	}
+	return tong;
+}
